73afcc02fa2b3a08f7743ee6cd0e8eecc7dbc146
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-11-23 20:37:12 +0100

    Dodano rozwiązanie zadania 1.

---
+++ Lab0/1.py
@@ -0,0 +1,16 @@
+"""
+Zadanie 1
+
+type(1+2)      ---> <class 'int'>
+type(1+4.5)    ---> <class 'float'>
+type(3/2)      ---> <class 'float'>
+type(4/2)      ---> <class 'float'>
+type(3//2)     ---> <class 'int'>
+type(-3//2)    ---> <class 'int'>
+type(11%2)     ---> <class 'int'>
+type(2**10)    ---> <class 'int'>
+type(8**(1/3)) ---> <class 'float'>
+
+"""
+
+
