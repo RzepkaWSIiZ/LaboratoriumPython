277cffe7311e0c39daadfae9cc0865406e851890
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-11-23 22:53:28 +0100

    Rozwiązanie zadania 4.

---
+++ Lab1/4.py
@@ -0,0 +1,13 @@
+import math
+
+a = int(input('Podaj współczynnik a: '))
+b = int(input('Podaj współczynnik b: '))
+c = int(input('Podaj współczynnik c: '))
+
+delta = (b**2)-4*a*c
+print(f'Delta wynosi: {delta}')
+
+print(f'x1 wynosi: {(-b - math.sqrt(delta)) / 2*a }')
+print(f'x2 wynosi: {(-b + math.sqrt(delta)) / 2*a }')
+
+
