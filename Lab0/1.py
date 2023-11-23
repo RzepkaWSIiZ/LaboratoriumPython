cc9669b75876bbfb6b709ef1b9f4b023b475597d
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-11-23 21:18:01 +0100

    Dodano rozwiązanie zadania 5.

--- Lab0/1.py
+++ Lab0/1.py
@@ -20,6 +20,7 @@ float(3.0)  ---> 3.0
 float("3")  ---> 3.0
 str(12.4)   ---> '12.4'
 bool(0)     ---> False
+"""
 #Zadanie 3
 
 a = int(input('Proszę podać bok a prostokąta: '))
@@ -39,10 +40,30 @@ zuzycie_paliwa = (spalanie/100)*droga
 print(f'Przewidywane zużycie paliwa: {zuzycie_paliwa}')
 print(f'Szacowany koszt podróży wynosi {cena_paliwa*zuzycie_paliwa}')
 
-"""
+
 import random
 #Zadanie 4.1
 
+droga = random.randint(1,100000)
+spalanie = float(input('Podaj spalanie samochodu l/100km: '))
+
+cena_paliwa = 6.5
+
+zuzycie_paliwa = (spalanie/100)*droga
+print(f'Przewidywane zużycie paliwa: {zuzycie_paliwa}')
+print(f'Szacowany koszt podróży wynosi {cena_paliwa*zuzycie_paliwa}')
+
+#Zadanie 5
+
+
+#3 edit
+a = int(input('Proszę podać bok a prostokąta: '))
+b = int(input('Proszę podać bok b prostokąta: '))
+
+print(f'Pole prostokąta wynosi: {a*b}')
+print(f'Obwód prostokąta wynosi: {2*a + 2*b}')
+
+#4 edit
 droga = random.randint(1,100000)
 spalanie = float(input('Podaj spalanie samochodu l/100km: '))
 
