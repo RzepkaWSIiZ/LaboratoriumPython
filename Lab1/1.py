bab883a1d30c5904ac48dd7f0243436c436b0bcd
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-11-23 21:42:19 +0100

    Rozwiązanie zadania 1.

---
+++ Lab1/1.py
@@ -0,0 +1,13 @@
+wiek = int(input('Wprowadź wiek klienta: '))
+cena_biletu = 0
+
+if wiek < 4:
+    print('Wstęp bezpłatny :)')
+elif 4 <= wiek <= 18:
+    cena_biletu += 10
+    print(f'Cena biletu wynosi: {cena_biletu}')
+else:
+    cena_biletu += 20
+    print(f'Cena biletu wynosi: {cena_biletu}')
+
+
