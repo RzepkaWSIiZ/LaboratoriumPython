c8d0a29bebe3016cad80abfdf06a89565c4506d9
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-12-10 20:05:49 +0100

    Rozwiązanie zadania drugiego łańcuchy.

---
+++ Lab2/2_łańcuchy.py
@@ -0,0 +1,16 @@
+#Zadanie 2 łańcuchy
+
+text = "Python jest super"
+
+#a
+print(text[0])
+#b
+print(text[-1])
+#c
+print(text[0:-1:2])
+#d
+print(text[1:-1:3])
+#e
+print(text[10:])
+#f
+print(text[::-1])
