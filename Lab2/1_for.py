3b7deded1496112b14fab191768e13beac7f285c
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-12-10 18:56:10 +0100

    Rozwiązanie zadania pierwszego pętli for.

---
+++ Lab2/1_for.py
@@ -0,0 +1,13 @@
+#Zadanie 1 for
+
+#for i in range(1,101):
+#    print(i, end=" ")
+
+#for i in range(100,-1, -1):
+#    print(i,end=" ")
+
+#for i in range(7,78,7):
+#    print(i, end=" ")
+
+for i in range(20,-1,-2):
+    print(i,end=" ")
