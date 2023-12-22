22eca4dfe6b2cca5c5d7dcca68b4395eee0cdd7d
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-12-22 21:50:31 +0100

    Rozwiązanie zadania trzeciego z pierwszej części.

---
+++ Lab3/3.py
@@ -0,0 +1,19 @@
+#Zadanie trzecie z częsci pierwszej
+
+tekst = "Python jest super"
+
+#a
+print(tekst[0])
+print(tekst[-1])
+
+#b
+print(tekst[::2])
+
+#c
+print(tekst[1:-1:3])
+
+#d
+print(tekst[10:-1])
+
+#e
+print(tekst[::-1])
