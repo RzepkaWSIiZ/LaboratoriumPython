609a2600000f79b585d507e807cdedfa3df5bc98
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2023-12-22 21:34:33 +0100

    Rozwiązanie zadania pierwszego z pierwszej części.

---
+++ Lab3/1.py
@@ -0,0 +1,22 @@
+#Zadanie pierwsze z częsci pierwszej
+
+list = ["Andrzej","Mariusz","Agata","Mariola"]
+
+#a
+list.sort()
+print(list)
+
+#b
+list.append("Grażyna")
+list.append("Małgorzata")
+
+print(list)
+
+#c
+list.insert(2,"Karol")
+print(list)
+
+#d
+list.reverse()
+lista_zdublowana = list*2
+print(lista_zdublowana)
