95313a6013481ab6fe58374da958d3c708498add
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2024-01-13 19:54:13 +0100

    Rozwiązanie zadania 4.

---
+++ Lab4/Zadanie_4.py
@@ -0,0 +1,10 @@
+#Zadanie 4
+def czy_dodatnia(x):
+    if x > 0:
+        print(f"Liczba {x} jest dodatnia.")
+    else:
+        print(f"Liczba {x} jest ujemna.")
+
+czy_dodatnia(9)
+
+czy_dodatnia(-78)
