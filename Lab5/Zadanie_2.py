465e58fbcdc53a574f945c193bd5d5ae0983ee30
RzepkaWSIiZ <w69841@student.wsiz.edu.pl>
2024-01-17 16:51:14 +0100

    Rozwiązanie zadania drugiego.

---
+++ Lab5/Zadanie_2.py
@@ -0,0 +1,18 @@
+#Zadanie 2
+
+import math
+import cmath
+#a)
+print(f"Pierwiastek drugiego stopnia z liczby 81 to: {float(math.sqrt(81))}")
+
+#b)
+print(f"8 do potęgi 10 jest równe: {int(math.pow(8,10))}")
+
+#c)
+print(f"Pierwiastek z 2 + pierwiastek z 3 + pierwiastek z 6 wynosi:{math.sqrt(2)+math.sqrt(3)+math.sqrt(6)}")
+
+#d)
+print(f"Pierwiastek drugiego stopnia z liczby -5 wynosi: {cmath.sqrt(-5)}")
+
+#e)
+print(f"Pierwiastek 3 stopnia z liczby 125 jest równy: {pow(125, 1/3)}")
